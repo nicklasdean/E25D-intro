8af9a3c57a5f6aa9d8826e0f0bafca2986645bbf
dean <nicklasfrederiksen@gmail.com>
2026-01-28 10:40:26 +0100

    First

--- src/main/java/arraylist/App.java
+++ src/main/java/arraylist/App.java
@@ -1,8 +1,20 @@
 package arraylist;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+        List<String> linkedNames = new LinkedList<String>();
+
+        getLengthOfList(names);
+        getLengthOfList(linkedNames);
+    }
+
+    private static int getLengthOfList(List<String> list){
+        int size = list.size();
+        return size;
     }
 }
